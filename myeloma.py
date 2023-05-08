b41467b0555c395a0d853628f5eb7b5f2fc59e57
giansalvo gusinu <profgusinu@gmail.com>
2023-05-08 14:57:14 +0200

    completed working version

---
+++ myeloma.py
@@ -0,0 +1,236 @@
+# MIT License
+#
+#   Copyright (c) 2023 Giansalvo Gusinu
+#   Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+# adapted from https://www.tensorflow.org/tutorials/keras/regression?hl=it#regression_using_a_dnn_and_multiple_inputs
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# FIELD_YEAR_OF_DIAGNOSIS = "Year of diagnosis (1975-2019 by 5)"
+# FIELD_PATIENT_ID = "Patient ID"
+# FIELD_YEAR_FOLLOW_UP = "Year of follow-up recode"
+# FIELD_YEAR_DEATH = "Year of death recode"
+# FIELD_AGE = "Age recode with single ages and 90+"
+# FIELD_SEQUENCE = "Sequence number"
+# FIELD_SEER_CAUSE_SPECIFIC = "SEER cause-specific death classification"
+# FIELD_SEER_OTHER_CAUSE = "SEER other cause of death classification"
+# FIELD_SURVIVAL = "Survival months"
+# FIELD_COD_TO_SITE = "COD to site recode"
+# FIELD_SURVIVAL_MONTHS_FLAG =  "Survival months flag"
+# # COD to site rec KM
+# # COD to site recode ICD-0-3-2023 Revision
+# # COD to site recode ICD-O-3 2023 Revision Expanded (1999+)
+# FIELD_VITAL_STATUS = "Vital status recode (study cutoff used)"
+# FIELD_RADIATION = "Radiation recode"
+# FIELD_CHEMOTHERAPY = "Chemotherapy recode (yes, no/unk)"
+# FIELD_MONTHS_FROM_DIAG_TO_TREAT = "Months from diagnosis to treatment"
+# FIELD_PRIMARY_SITE = "Primary Site - labeled"
+# FIELD_HIST_BEHAV = "ICD-O-3 Hist/behav, malignant"
+
+
+FIELD_YEAR_OF_DIAGNOSIS = "A"
+FIELD_YEAR_DEATH = "B"
+FIELD_AGE = "C"
+FIELD_SEER_CAUSE_SPECIFIC = "D"
+FIELD_SEER_OTHER_CAUSE = "E"
+FIELD_SURVIVAL = "F"
+FIELD_COD_TO_SITE = "G"
+FIELD_MONTHS_FROM_DIAG_TO_TREAT = "H"
+
+def one_hot_encoding(dataset, FIELD_COD_TO_SITE):
+    ohe = OneHotEncoder()
+    transformed = ohe.fit_transform(dataset[[FIELD_COD_TO_SITE]])
+    dataset[ohe.categories_[0]] = transformed.toarray()
+    dataset[ohe.categories_[0]] = dataset[ohe.categories_[0]].astype(int)  # convert to int
+    dataset.drop(FIELD_COD_TO_SITE, axis=1, inplace=True)
+    return dataset
+
+
+def normalization(train_dataset, train_features):
+    #see how different the ranges of each feature are
+    print(train_dataset.describe().transpose()[['mean', 'std']])
+
+    normalizer = tf.keras.layers.Normalization(axis=-1)
+    temp = np.asarray(train_features)
+    print("temp " + str(temp.shape))
+    normalizer.adapt(temp)
+    # Check normalization on screen
+    print("Normalizer: " + str(normalizer.mean.numpy()))
+    return normalizer
+
+
+def build_and_compile_model(norm):
+  model = keras.Sequential([
+      norm,
+      layers.Dense(32, activation='relu'),
+      layers.Dense(32, activation='relu'),
+      layers.Dense(1)
+  ])
+
+  model.compile(loss='mean_absolute_error',
+                optimizer=tf.keras.optimizers.Adam(0.001))
+  return model
+
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.ylim([0, 10])
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [SURVIVAL]')
+  plt.legend()
+  plt.grid(True)
+  plt.savefig("myeloma_plot_loss")
+  plt.close()
+
+
+def plot_scatter(x, y, train_features, train_labels):
+  plt.scatter(train_features['Survival'], train_labels, label='Data')
+  plt.plot(x, y, color='k', label='Predictions')
+  plt.xlabel('XXX')
+  plt.ylabel('YYY')
+  plt.legend()
+  plt.savefig("myeloma_scatter_train.png")
+  plt.close()
+
+
+
+
+
+def main():
+    # Make NumPy printouts easier to read.
+    np.set_printoptions(precision=3, suppress=True)
+    print(tf.__version__)
+
+    column_names = [FIELD_YEAR_OF_DIAGNOSIS,
+                    FIELD_YEAR_DEATH,
+                    FIELD_AGE,
+                    FIELD_SEER_CAUSE_SPECIFIC,
+                    FIELD_SEER_OTHER_CAUSE,
+                    FIELD_SURVIVAL,
+                    FIELD_COD_TO_SITE,
+                    FIELD_MONTHS_FROM_DIAG_TO_TREAT]
+
+    raw_dataset = pd.read_csv("myeloma_work.csv", names=column_names,
+                              na_values='?', comment='\t', header=1,
+                              sep=';', skipinitialspace=True)
+    dataset = raw_dataset.copy()
+    print("Dataset shape: " + str(dataset.shape))
+    print("Dataset head:")
+    print(dataset.head())
+    print("Null values:")
+    print(dataset.isna().sum())
+
+    # pandas drop empy records and colum in dataframe
+    dataset = dataset.dropna()
+
+    # ONE HOT ENCODER COD_TO_SITE
+    dataset = one_hot_encoding(dataset, FIELD_COD_TO_SITE)
+
+    # split dataset train and test
+    train_dataset = dataset.sample(frac=0.8, random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+
+    # Visual inspection of the data
+    sns.pairplot(train_dataset[[FIELD_YEAR_OF_DIAGNOSIS,
+                                FIELD_YEAR_DEATH,
+                                FIELD_AGE,
+                                FIELD_SEER_CAUSE_SPECIFIC,
+                                FIELD_SEER_OTHER_CAUSE,
+                                FIELD_SURVIVAL]], diag_kind='kde')
+    plt.savefig("myeloma_compare.png")
+    plt.close()
+
+    print("Analytic analysis of the dataset:")
+    print(train_dataset.describe().transpose())
+
+    #  Split features from labels
+    train_features = train_dataset.copy()
+    test_features = test_dataset.copy()
+
+    # remove column "survival"
+    train_labels = train_features.pop(FIELD_SURVIVAL)
+    test_labels = test_features.pop(FIELD_SURVIVAL)
+    print("train_label " + str(train_labels.shape))
+    print("test_labels " + str(test_labels.shape))
+
+    # Normalizzation
+    normalizer = normalization(train_dataset, train_features)
+
+    dnn_model = build_and_compile_model(normalizer)
+    dnn_model.summary()
+
+    history = dnn_model.fit(
+        train_features,
+        train_labels,
+        validation_split=0.2,
+        verbose=0, epochs=100)
+    plot_loss(history)
+
+    # TODO ERROR
+    # x = tf.linspace(0.0, 250, 251)
+    # y = dnn_model.predict(x)
+    # plot_scatter(x,y, train_features, train_labels)
+
+    # evaluate on test set
+    test_results = {}
+    test_results['dnn_model'] = dnn_model.evaluate(
+        test_features, test_labels, verbose=0)
+    print("Test_result " + str(test_results))
+    #test set performance
+    print(pd.DataFrame(test_results, index=['Mean absolute error [SURVIVAL]']).T)
+
+    #make predictions on the testset and plot
+    test_predictions = dnn_model.predict(test_features).flatten()
+    a = plt.axes(aspect='equal')
+    plt.scatter(test_labels, test_predictions)
+    plt.xlabel('True Values [Survival]')
+    plt.ylabel('Predictions [Survival]')
+    lims = [0, 50]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    _ = plt.plot(lims, lims)
+    plt.savefig("myeloma_x_y.png")
+    plt.close()
+
+    #check the error distribution:
+    error = test_predictions - test_labels
+    plt.hist(error, bins=25)
+    plt.xlabel('Prediction Error [Survival]')
+    _ = plt.ylabel('Count')
+    plt.savefig("myeloma_error_distrib.png")
+
+    #save the model for future use
+    dnn_model.save('dnn_model.keras')
+
+    #reuse the saved model
+    # reloaded = tf.keras.models.load_model('dnn_model.keras')
+    # test_results['reloaded'] = reloaded.evaluate(
+    #     test_features, test_labels, verbose=0)
+
+if __name__ == '__main__':
+    main()
