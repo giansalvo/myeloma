ec6e0987e6bb36d6f7d833b3052ac230abc31f23
giansalvo gusinu <profgusinu@gmail.com>
2023-05-31 10:13:13 +0200

    aestetic changes

--- myeloma.R
+++ myeloma.R
@@ -1,3 +1,26 @@
+"""
+    Models for estimate the survival time period for Myeloma affected patients
+
+    Copyright (c) 2023 Giansalvo Gusinu
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
 #load packages
 #install.packages("car")
 library(car)
@@ -11,24 +34,31 @@ ds <- read.table("myeloma_work_R.csv", header = T, sep = ";")
 head(ds, c(3, 7))
 
 plot(ds$survival, ds$age)
-plot(ds$year_diagnosis, ds$death, pch=16, col = "blue")
+plot(ds$year_diagnosis, ds$death, pch=16)
 
 hist(ds$survival)
+hist(ds$death)
+hist(ds$age)
+hist(ds$year_diagnosis)
+hist(ds$death_specific)
+hist(ds$diagnosis_to_treat)
 
-boxplot(ds$age)
+boxplot(ds$survival)
 boxplot(ds$death)
+boxplot(ds$age)
+boxplot(ds$year_diagnosis)
+boxplot(ds$death_specific)
+boxplot(ds$diagnosis_to_treat)
 
 boxplot(ds$age ~ ds$death_specific)
 boxplot(ds$death ~ ds$death_specific)
 
-
 survival.glm<-glm(survival ~ age + death + death_specific + death_other, 
                   data = ds)
 
-
 summary(survival.glm)
 
-plot(survival.glm$residuals, pch = 16, col = "red")
+plot(survival.glm$residuals, pch = 16)
 
 anova(survival.glm)
 
@@ -62,3 +92,4 @@ ggplot(ggpredict(model1, terms = c("age [1,5,10]", "death", "year_diagnosis")),
 ########################
 model1 <- lm(data = ds, survival ~ age + death + year_diagnosis)
 plot_model(model1, type = 'diag')
+
