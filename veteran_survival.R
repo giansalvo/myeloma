efa3ca2f9c070209601c858ad7da328abfbbf9a6
giansalvo gusinu <profgusinu@gmail.com>
2023-06-16 12:50:43 +0200

    survival in R

---
+++ veteran_survival.R
@@ -0,0 +1,92 @@
+#https://rviews.rstudio.com/2017/09/25/survival-analysis-with-r/
+
+library(survival)
+library(ranger)
+library(ggplot2)
+library(dplyr)
+library(ggfortify)
+
+data(veteran)
+head(veteran)
+
+# Kaplan Meier Survival Curve
+km <- with(veteran, Surv(time, status))
+head(km,80)
+km_fit <- survfit(Surv(time, status) ~ 1, data=veteran)
+summary(km_fit, times = c(1,30,60,90*(1:10)))
+
+#plot(km_fit, xlab="Days", main = 'Kaplan Meyer Plot') #base graphics is always ready
+autoplot(km_fit)
+
+#Next, we look at survival curves by treatment.
+km_trt_fit <- survfit(Surv(time, status) ~ trt, data=veteran)
+autoplot(km_trt_fit)
+
+#look at survival by age
+vet <- mutate(veteran, AG = ifelse((age < 60), "LT60", "OV60"),
+              AG = factor(AG),
+              trt = factor(trt,labels=c("standard","test")),
+              prior = factor(prior,labels=c("N0","Yes")))
+
+km_AG_fit <- survfit(Surv(time, status) ~ AG, data=vet)
+autoplot(km_AG_fit)
+
+# Fit Cox Model
+cox <- coxph(Surv(time, status) ~ trt + celltype + karno                   + diagtime + age + prior , data = vet)
+summary(cox)
+
+cox_fit <- survfit(cox)
+#plot(cox_fit, main = "cph model", xlab="Days")
+autoplot(cox_fit)
+
+# The plots show how the effects of the covariates change over time. Notice the steep slope and then abrupt change in slope of karno.
+aa_fit <-aareg(Surv(time, status) ~ trt + celltype +
+                 karno + diagtime + age + prior , 
+               data = vet)
+aa_fit
+
+#summary(aa_fit)  # provides a more complete summary of results
+autoplot(aa_fit)
+
+###########################################################
+############################################################
+#Random Forests Model
+# ranger model
+r_fit <- ranger(Surv(time, status) ~ trt + celltype + 
+                  karno + diagtime + age + prior,
+                data = vet,
+                mtry = 4,
+                importance = "permutation",
+                splitrule = "extratrees",
+                verbose = TRUE)
+
+# Average the survival models
+death_times <- r_fit$unique.death.times 
+surv_prob <- data.frame(r_fit$survival)
+avg_prob <- sapply(surv_prob,mean)
+
+# Plot the survival models for each patient
+plot(r_fit$unique.death.times,r_fit$survival[1,], 
+     type = "l", 
+     ylim = c(0,1),
+     col = "red",
+     xlab = "Days",
+     ylab = "survival",
+     main = "Patient Survival Curves")
+
+#
+cols <- colors()
+for (n in sample(c(2:dim(vet)[1]), 20)){
+  lines(r_fit$unique.death.times, r_fit$survival[n,], type = "l", col = cols[n])
+}
+lines(death_times, avg_prob, lwd = 2)
+legend(500, 0.7, legend = c('Average = black'))
+
+vi <- data.frame(sort(round(r_fit$variable.importance, 4), decreasing = TRUE))
+names(vi) <- "importance"
+head(vi)
+     
+     
+
+
+
